0939432c30fe2a6b67e6161efa753b7776199b81
hectorag <hector-av1@hotmail.com>
2023-08-16 01:26:26 -0700

    button styles and dice img

--- src/App.vue
+++ src/App.vue
@@ -14,13 +14,16 @@
       <picture class="advice__divider">
         <source 
         srcset="./assets/images/pattern-divider-desktop.svg"
-        media="(min-width: 600px)">
+        media="(min-width: 800px)">
 
         <img src="./assets/images/pattern-divider-mobile.svg" alt="Image that devides the quote andd the button to generate another quote">
       </picture>
 
+      <button class="button">
+        <img src="./assets/images/icon-dice.svg" alt="Image that devides the quote andd the button to generate another quote">
+      </button>
       
     </div>
-    <button>Dice</button>
+    
   </main>
 </template>
