f96573992796465c3e9d6e90fdfb13f3838fdf1e
hectorag <hector-av1@hotmail.com>
2023-08-15 22:56:27 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    Advice #<!-- Advice ID goes here -->
+
+      "<!-- Advice text goes here -->"
+  </main>
+</template>
