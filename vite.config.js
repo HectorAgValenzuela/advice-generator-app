0f1c4f68f389259f244f8f92497a80e3fb1c5fd2
hectorag <hector-av1@hotmail.com>
2023-08-17 01:18:27 -0700

    adding slash to base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'deploy-advice-generator-app',
+  base: '/deploy-advice-generator-app',
   plugins: [
     vue(),
   ],
