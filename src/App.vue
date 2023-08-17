a162b66a81ed854ce4f908549c01a0f59a2d9131
hectorag <hector-av1@hotmail.com>
2023-08-17 00:14:51 -0700

    crating a method to call the api

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,56 @@
-<script setup>
-
-</script>
-
 <template>
   <main>
     <div class="advice">
 
-      <h1 class="advice__title">Advice #117</h1>
+      <h1 class="advice__title">Advice #{{ adviceID }}</h1>
       
-      <p class="advice__content">"It is easy to sit up and take notice, what's 
-        difficult is getting up and taking action"</p>
+      <p class="advice__content">{{ advice }}</p>
       
       <picture class="advice__divider">
         <source 
         srcset="./assets/images/pattern-divider-desktop.svg"
         media="(min-width: 800px)">
 
-        <img src="./assets/images/pattern-divider-mobile.svg" alt="Image that devides the quote andd the button to generate another quote">
+        <img src="./assets/images/pattern-divider-mobile.svg" 
+            alt="Image that devides the quote andd the button to generate another quote">
       </picture>
 
-      <button class="button">
-        <img src="./assets/images/icon-dice.svg" alt="Image that devides the quote andd the button to generate another quote">
+      <button class="button" @click="getAdvice">
+        <img src="./assets/images/icon-dice.svg" 
+            alt="Image that devides the quote andd the button to generate another quote">
       </button>
       
     </div>
     
   </main>
 </template>
+
+<script>
+  import axios from 'axios';
+
+  export default {
+    data() {
+      return {
+        advice: '',
+        adviceID: 0,
+      }
+    },
+    mounted() {
+      this.getAdvice()
+    },
+     methods: {
+      async getAdvice() {
+        let randNum = Math.floor( Math.random() * 224); //Setting the max to 224
+        await axios.get(`https://api.adviceslip.com/advice/${randNum}`)
+          .then(response => {
+            console.log(response)
+            this.adviceID = response.data.slip.id;
+            this.advice = response.data.slip.advice;
+          })
+          .catch(error => {
+            console.log(error);
+        });
+      }
+     }
+  }
+</script>
